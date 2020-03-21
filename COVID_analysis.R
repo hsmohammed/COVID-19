2910570d20814e325d85fb064cad39003c6e9641
Hossameldin <hos40044@mail.ubc.ca>
2020-03-20 17:19:00 -0700

    add files

--- COVID_analysis.R
+++ COVID_analysis.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(forecast)
 library(zoo)
 library(xts)
+library(gridExtra)
+library(gghighlight)
 
 # read data
 
@@ -15,17 +17,17 @@ COVID_recovered <-  read_csv("csse_covid_19_data/csse_covid_19_time_series/time_
 # pivot_longer
 
 COVID_confirmed <- COVID_confirmed %>% 
-  pivot_longer(cols      = c('1/22/20':'3/17/20'),
+  pivot_longer(cols      = c('1/22/20':'3/18/20'),
                names_to  = "date",
                values_to = "n_cases")
 
 COVID_deaths <- COVID_deaths %>% 
-  pivot_longer(cols      = c('1/22/20':'3/17/20'),
+  pivot_longer(cols      = c('1/22/20':'3/18/20'),
                names_to  = "date",
                values_to = "n_cases")
 
 COVID_recovered <- COVID_recovered %>% 
-  pivot_longer(cols      = c('1/22/20':'3/17/20'),
+  pivot_longer(cols      = c('1/22/20':'3/18/20'),
                names_to  = "date",
                values_to = "n_cases")
 
@@ -39,6 +41,72 @@ colnames(COVID_recovered) <-  c('state', 'country', 'lat', 'long','date', 'n_cas
 COVID_confirmed <- COVID_confirmed %>% group_by(country, date) %>% summarise(n_cases=sum(n_cases))
 COVID_deaths <- COVID_deaths %>% group_by(country, date) %>% summarise(n_cases=sum(n_cases))
 COVID_recovered <- COVID_recovered %>% group_by(country, date) %>% summarise(n_cases=sum(n_cases))
+
+COVID_confirmed$date <- as.Date(COVID_confirmed$date, format = '%m/%d/%Y')
+COVID_deaths$date <- as.Date(COVID_confirmed$date, format = '%m/%d/%Y')
+COVID_recovered$date <- as.Date(COVID_confirmed$date, format = '%m/%d/%Y')
+
+
+# Viz
+COVID_confirmed_viz <- COVID_confirmed %>% 
+  dplyr::filter(country %in% c("Iran", "US", "Italy", "Spain")) %>% 
+  group_by(country) %>% 
+  arrange(country, date) %>% 
+  dplyr::filter(date >= "0020-02-15")
+
+COVID_deaths_viz <- COVID_deaths %>% 
+  dplyr::filter(country %in% c("Iran", "US", "Italy", "Spain")) %>% 
+  group_by(country) %>% 
+  arrange(country, date) %>% 
+  dplyr::filter(date >= "0020-02-15")
+
+COVID_recovered_viz <- COVID_recovered %>% 
+  dplyr::filter(country %in% c("Iran", "US", "Italy", "Spain")) %>% 
+  group_by(country) %>% 
+  arrange(country, date) %>% 
+  dplyr::filter(date >= "0020-02-20")
+
+COVID_confirmed_viz1 <- COVID_confirmed %>% 
+  dplyr::filter(country == "Korea, South") %>% 
+  group_by(country) %>% 
+  arrange(country, date) %>% 
+  dplyr::filter(date >= "0020-02-20")
+
+COVID_deaths_viz1 <- COVID_deaths %>% 
+  dplyr::filter(country == "Korea, South") %>% 
+  group_by(country) %>% 
+  arrange(country, date) %>% 
+  dplyr::filter(date >= "0020-02-20")
+
+COVID_recovered_viz1 <- COVID_recovered %>% 
+  dplyr::filter(country == "Korea, South") %>% 
+  group_by(country) %>% 
+  arrange(country, date) %>% 
+  dplyr::filter(date >= "0020-02-20")
+
+p1 <- ggplot()+
+  geom_line(data = COVID_confirmed_viz, aes(date, n_cases, group = country, color = country), size = 1)+
+  geom_line(data = COVID_confirmed_viz1, aes(date, n_cases, group = country, color = country), size = 3, linetype = "dashed")+
+  ylab("n")+
+  theme_bw()+
+  ggtitle("Confirmed cases")
+
+p2 <- ggplot()+
+  geom_line(data = COVID_deaths_viz, aes(date, n_cases, group = country, color = country), size = 1)+
+  geom_line(data = COVID_deaths_viz1, aes(date, n_cases, group = country, color = country), size = 3, linetype = "dashed")+
+  ylab("n")+
+  theme_bw()+
+  ggtitle("Deaths")
+
+p3 <- ggplot()+
+  geom_line(data = COVID_recovered_viz, aes(date, n_cases, group = country, color = country), size = 1)+
+  geom_line(data = COVID_recovered_viz1, aes(date, n_cases, group = country, color = country), size = 3, linetype = "dashed")+
+  ylab("n")+
+  theme_bw()+
+  ggtitle("Recovered")
+
+
+grid.arrange(p1,p2,p3)
 
 # pivot_wider
 
@@ -64,29 +132,62 @@ COVID_recovered <- COVID_recovered %>%
 
 # countries
 
+COVID_confirmed_China <- xts(x = COVID_confirmed$China, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
+COVID_confirmed_Italy <- xts(x = COVID_confirmed$Italy, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
+COVID_confirmed_Iran <- xts(x = COVID_confirmed$Iran, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+
+COVID_confirmed_Spain <- xts(x = COVID_confirmed$Spain, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+
+COVID_confirmed_Germany <- xts(x = COVID_confirmed$Germany, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
 COVID_confirmed_US <- xts(x = COVID_confirmed$US, 
-                                    order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
-COVID_confirmed_China <- xts(x = COVID_confirmed$China, 
-                        order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+COVID_confirmed_France <- xts(x = COVID_confirmed$France, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+
+COVID_confirmed_SouthKorea <- xts(x = COVID_confirmed$`Korea, South`, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+
+COVID_confirmed_Switzerland <- xts(x = COVID_confirmed$Switzerland, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
+
+COVID_confirmed_UK <- xts(x = COVID_confirmed$`United Kingdom`, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
 COVID_confirmed_Canada <- xts(x = COVID_confirmed$Canada, 
-                           order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
-
-plot(COVID_confirmed_Canada)
-
-Acf(COVID_confirmed_US)
-
-Acf(VID_confirmed_China)
+                          order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
 
 
-a
+COVID_confirmed_Egypt <- xts(x = COVID_confirmed$Egypt, 
+                             order.by = as.Date(COVID_confirmed$date,format = '%m/%d/%Y'))
 
-ggplot(fortify(COVID_confirmed_ts),aes(Index,COVID_confirmed_USA_ts))+geom_line()
+
+
+
+
+
+ggplot()+
+  # geom_line(data = fortify(COVID_confirmed_China),aes(Index,COVID_confirmed_China), color = 'red')+
+  # geom_line(data = fortify(COVID_confirmed_Italy),aes(Index,COVID_confirmed_Italy), color = "blue")+
+  # geom_line(data = fortify(COVID_confirmed_Iran),aes(Index,COVID_confirmed_Iran), color ="orange")+
+  # geom_line(data = fortify(COVID_confirmed_Spain),aes(Index,COVID_confirmed_Spain), color = "cyan")+
+  # geom_line(data = fortify(COVID_confirmed_Germany),aes(Index,COVID_confirmed_Germany), color = "green")+
+  geom_line(data = fortify(COVID_confirmed_US),aes(Index,COVID_confirmed_US), color = "black")+
+  geom_line(data = fortify(COVID_confirmed_France),aes(Index,COVID_confirmed_France), color = "pink")+
+  geom_line(data = fortify(COVID_confirmed_SouthKorea),aes(Index,COVID_confirmed_SouthKorea))+
+  geom_line(data = fortify(COVID_confirmed_Switzerland),aes(Index,COVID_confirmed_Switzerland))+
+  geom_line(data = fortify(COVID_confirmed_UK),aes(Index,COVID_confirmed_UK))+
+  geom_line(data = fortify(COVID_confirmed_Canada),aes(Index,COVID_confirmed_Canada))+
+  geom_line(data = fortify(COVID_confirmed_Egypt),aes(Index,COVID_confirmed_Egypt))
 
 # Create dates
 dates <- as.Date("2016-01-01") + 0:4
